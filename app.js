9974033fc41b0587c7d8686c6b0c6b187a5a8154
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-07-16 20:25:38 +0900

    setTimeoutを簡易的に実装

--- app.js
+++ app.js
@@ -3,4 +3,11 @@
 function init() {
     let element = document.getElementById("js-test")
     element.innerHTML = "js-test-text"
+    animation_test(element, 1000)
+}
+
+function animation_test(element, time) {
+    setTimeout(function() {
+        element.innerHTML += `${time}ミリ秒経過`
+    }, time)
 }

080de8b8717ceca99a6746f492cba37427ead207
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-07-17 07:15:58 +0900

    タイマー関数を実装

--- app.js
+++ app.js
@@ -3,11 +3,26 @@
 function init() {
     let element = document.getElementById("js-test")
     element.innerHTML = "js-test-text"
-    animation_test(element, 1000)
+    cout_test(element, 200)
 }
 
 function animation_test(element, time) {
     setTimeout(function() {
         element.innerHTML += `${time}ミリ秒経過`
     }, time)
+}
+
+function cout_test(element, end) {
+    let i = 0
+    // setInterval( fun, 間隔（ミリ秒）)
+    let id = setInterval(function() {
+        i++
+        element.innerHTML = `${i}`
+        
+        if (i >= end) {
+            console.log("end");
+            clearInterval(id)
+            return
+        }
+    }, 1000/50) //一秒(1000ミリ秒)間に50回更新（50フレーム）
 }

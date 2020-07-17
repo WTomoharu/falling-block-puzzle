341b013429cf85158a4696a7453e28af5428edec
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-07-17 14:06:43 +0900

    回転を実装

--- app.js
+++ app.js
@@ -29,13 +29,11 @@ function cout_test(element, end) {
 function move_test(element, time/* ミリ秒 */, dist, dir = "top") {
     let zentai = time / 20
     let i = 0
-    let textObj = document.getElementById("js-test")
     let moto = Number(element.style[dir].slice(0, -2))
     let id = setInterval(function () {
         i++
         console.log(moto + ((i / zentai) * dist));
         element.style[dir] = moto + ((i / zentai) * dist)
-        // textObj.innerHTML = `${i}:${(i / zentai) * dist}`
         if (i >= zentai) {
             clearInterval(id)
             return
@@ -49,5 +47,26 @@ document.addEventListener("keydown", (evt) => {
         move_test(document.getElementById("rect-svg"), 200, 100, "left")
     } else if (evt.key == "ArrowLeft") {
         move_test(document.getElementById("rect-svg"), 200, -100, "left")
+    } else if (evt.key == "ArrowUp") {
+        rotation(document.getElementById("rect-svg"), 200, 90)
     }
 }) 
+
+function rotation(element, time, angle) {
+    let zentai = time / 20
+    let i = 0
+    // let moto = Number(element.style.transform)
+    let moto = element.style.transform.match(/\d+?/gi) || [0]
+    moto = Number(moto.join(""))
+    console.log(moto);
+    let id = setInterval(function () {
+        i++
+        // console.log(((i / zentai) * angle));
+        element.style.transform = `rotate(${moto + ((i / zentai) * angle)}deg)`
+        // textObj.innerHTML = `${i}:${(i / zentai) * dist}`
+        if (i >= zentai) {
+            clearInterval(id)
+            return
+        }
+    }, 1000 / 50)
+}

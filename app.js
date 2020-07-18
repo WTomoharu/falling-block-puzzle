51a231eead1360d7f9065d4fba3cbf3ec57e6f27
Tomoharu Watanabe <watatomo1216@gmail.com>
2020-07-18 12:15:06 +0900

    回転軸のズレを修正する機能を実装

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 "use strict";
 
+const rotat_dict = {
+    "T": [
+        { x: -25, y: 25 },
+        { x: 25, y: -25 },
+        { x: 25, y: 25 },
+        { x: -25, y: -25 }
+    ]
+}
+
 let ins;
 
 function init() {
     let element = document.getElementById("rect-svg")
     ins = new Block(element)
 }
+
+// 常に余算が正になるような関数
+// function mod(i, j) {
+//     return (i % j) < 0 ? (i % j) + 0 + (j < 0 ? -j : j) : (i % j + 0);
+// }
 
 document.addEventListener("keydown", (evt) => {
     console.log(evt.key);
@@ -21,16 +35,17 @@ document.addEventListener("keydown", (evt) => {
 })
 
 class Block {
-    constructor(element, block_type = "") {
+    constructor(element, block_type = "T") {
         this.element = element
         this.block_type = block_type
+        this.rotat_num = 0
 
-        this.move_improve(10000, 0, 600)
+        // this.move_improve(10000, 0, 600)
     }
 
-    move_improve(time/* ミリ秒 */, x, y, calcallback = null) {
+    move_improve(time/*ミリ秒*/, x, y, calcallback = null) {
         let start = Date.now()
-        console.log(`${(x / (time / 20))}:${(y / (time / 20))}`);
+        // console.log(`${(x / (time / 20))}:${(y / (time / 20))}`);
 
         let id = setInterval(() => {
             let time_passed = Date.now() - start
@@ -52,9 +67,22 @@ class Block {
         }, 20)
     }
 
-    rotat_improve(time/* ミリ秒 */, angle, calcallback = null) {
+    rotat_improve(time/*ミリ秒*/, angle, calcallback = null) {
         let start = Date.now()
         console.log(`angle=${(angle / (time / 20))}`);
+
+        // 回転軸のズレを修正
+        this.move_improve(time,
+            rotat_dict[this.block_type][this.rotat_num].x,
+            rotat_dict[this.block_type][this.rotat_num].y
+        )
+
+        // rotat_numを終了後の値にセット
+        if (angle >= 0) {
+            this.rotat_num = (this.rotat_num + 1) % 4
+        } else {
+            this.rotat_num = (this.rotat_num - 1) % 4
+        }
 
         let id = setInterval(() => {
             let time_passed = Date.now() - start
